27d4d1351451ec34ee7c004313669ee352dd33f4
Jakub Lenner <lennejak@fel.cvut.cz>
2025-09-23 13:33:35 +0200

    fixed plugin implementation

--- src/open_vins_plugin.cpp
+++ src/open_vins_plugin.cpp
@@ -1,6 +1,6 @@
 #include <mrs_uav_state_estimators/estimators/state/state_generic.h>
 
-namespace open_vins
+namespace mrs_open_vins_estimator_plugin
 {
 
 const char estimator_name[] = "open_vins";
@@ -8,7 +8,8 @@ const bool is_core_plugin = false;
 
 class OpenVins : public rclcpp::Node, public mrs_uav_state_estimators::StateGeneric {
 public:
-  OpenVins(rclcpp::NodeOptions options) : rclcpp::Node(estimator_name, options), mrs_uav_state_estimators::StateGeneric(estimator_name, is_core_plugin) {
+  OpenVins() : rclcpp::Node(estimator_name), mrs_uav_state_estimators::StateGeneric(estimator_name, is_core_plugin) {
+    RCLCPP_INFO(get_logger(), "OpenVins 1");
   }
 
   ~OpenVins(void) {
@@ -17,5 +18,7 @@ public:
 
 }  // namespace open_vins
 
-#include <rclcpp_components/register_node_macro.hpp>
-RCLCPP_COMPONENTS_REGISTER_NODE(open_vins::OpenVins);
+// #include <rclcpp_components/register_node_macro.hpp>
+// RCLCPP_COMPONENTS_REGISTER_NODE(open_vins::OpenVins);
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(mrs_open_vins_estimator_plugin::OpenVins, mrs_uav_managers::StateEstimator)
